f0485e31828d8371b7b3ba8554a0db890561d542
Mr. Amirbhai S. Kasiri <aamir.kashiri@gmail.com>
2023-06-01 11:24:34 +0530

    no update

--- lib/presentation/event/event_selection_screen.dart
+++ lib/presentation/event/event_selection_screen.dart
@@ -49,15 +49,9 @@ class _EventSelectionScreenState extends State<EventSelectionScreen> {
             height: 24,
           ),
           Center(
-<<<<<<< HEAD
             child: Container(
               padding: EdgeInsets.all(10),
-              width: MediaQuery.of(context).size.width * .70,
-=======
-            child: Container(    
-              padding: EdgeInsets.all(10),          
-              width: MediaQuery.of(context).size.width*.79,
->>>>>>> 6b3ef3738ac30e041df282dac4f2abcea36c47f8
+              width: MediaQuery.of(context).size.width * .79,
               height: 50,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
@@ -66,33 +60,26 @@ class _EventSelectionScreenState extends State<EventSelectionScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.search,color: ColorManager.lightGrey,),
+                  Icon(
+                    Icons.search,
+                    color: ColorManager.lightGrey,
+                  ),
                   Expanded(
-<<<<<<< HEAD
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "dadfkkd",
-                          hintStyle:
-                              TextStyle(fontSize: FontSize.appBarLabelSize),
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none),
-=======
                     child: SizedBox(
                       height: 100,
-                      child: TextFormField(                      
+                      child: TextFormField(
                         decoration: InputDecoration(
-                          
-                          contentPadding: EdgeInsets.all(6),
-                          hintText: AppStrings.eventSearchLabel,
-                          hintStyle: TextStyle(fontSize: FontSize.appBarLabelSize,color: ColorManager.lightGrey),
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none
-                          // isDense: true
-                        ),
+                            contentPadding: EdgeInsets.all(6),
+                            hintText: AppStrings.eventSearchLabel,
+                            hintStyle: TextStyle(
+                                fontSize: FontSize.appBarLabelSize,
+                                color: ColorManager.lightGrey),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none
+                            // isDense: true
+                            ),
                       ),
->>>>>>> 6b3ef3738ac30e041df282dac4f2abcea36c47f8
                     ),
                   ),
                 ],
